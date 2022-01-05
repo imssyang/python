10e06ee11478bb9b9128ac4bd6307e4ad3d5985f
imssyang <imssyang@gmail.com>
2022-01-05 15:11:21 +0800

    update sample

---
+++ sample/core/subprocess/process_process.py
@@ -0,0 +1,23 @@
+import time
+from multiprocessing import Process
+import os
+
+def info(title):
+    print(title)
+    print('module name:', __name__)
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid())
+
+def f(name):
+    time.sleep(10)
+    info('function f')
+    print('hello', name)
+
+if __name__ == '__main__':
+    info('main line')
+    p = Process(target=f, args=('bob',))
+    p.start()
+    time.sleep(3)
+    print(f"{p}")
+    #p.kill()
+    #p.join()
