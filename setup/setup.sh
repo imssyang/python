1da09f3a7704e3cc1a2da6031c60a50152c675b8
imssyang <imssyang@gmail.com>
2022-04-03 20:57:18 +0800

    add requirements.txt

--- setup/setup.sh
+++ setup/setup.sh
@@ -7,16 +7,15 @@ init() {
   if [[ ! -d $HOME/envs ]]; then
     mkdir $HOME/envs
     chmod 755 $HOME/envs
-	echo "mkdir $HOME/envs"
+    echo "mkdir $HOME/envs"
   fi
+
+  $HOME/bin/pip3 install --no-cache-dir -r $HOME/setup/requirements.txt
 }
 
 case "$1" in
-  init)
-    init
-    ;;
-  *)
-    SCRIPTNAME="${0##*/}"
+  init) init ;;
+  *) SCRIPTNAME="${0##*/}"
     echo "Usage: $SCRIPTNAME {init}"
     exit 3
     ;;
