4f7f6f4450392fcedff7012eafb3437e3e7234a6
imssyang <imssyang@gmail.com>
2022-04-22 01:10:46 +0800

    update python version

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,12 +3,46 @@
 APP=python3
 HOME=/opt/$APP
 
-init() {
-  if [[ ! -d $HOME/envs ]]; then
-    mkdir $HOME/envs
-    chmod 755 $HOME/envs
-    echo "mkdir $HOME/envs"
+_mkdir() {
+  name=$1
+  if [[ ! -d $name ]]; then
+    mkdir -p $name
   fi
+}
+
+_rmdir() {
+  name=$1
+  if [[ -d $name ]]; then
+    rm -rf $name
+  fi
+}
+
+_create_symlink() {
+  src=$1
+  dst=$2
+  if [[ ! -d $dst ]] && [[ ! -s $dst ]]; then
+    ln -s $src $dst
+    echo "($APP) create symlink: $src -> $dst"
+  fi
+}
+
+_delete_symlink() {
+  dst=$1
+  if [[ -d $dst ]] || [[ -s $dst ]]; then
+    rm -rf $dst
+    echo "($APP) delete symlink: $dst"
+  fi
+}
+
+init() {
+  _mkdir $HOME/envs
+
+  _create_symlink $HOME/bin/pip3     $HOME/bin/pip
+  _create_symlink $HOME/bin/python3  $HOME/bin/python
+  _create_symlink $HOME/bin/pydoc3   $HOME/bin/pydoc
+
+  chown -R root:root $HOME
+  chmod 755 $HOME
 
   $HOME/bin/pip3 install --no-cache-dir -r $HOME/setup/requirements.txt
 }
