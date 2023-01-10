1c3e94dce8a555a217e4bdd4fb025914a57968ee
imssyang <imssyang@gmail.com>
2023-01-10 16:41:19 +0800

    add datetime/basic

--- sample/core/datetime/basic.py
+++ sample/core/datetime/basic.py
@@ -1,6 +1,13 @@
 import time
 from datetime import datetime
 
-dt = datetime.fromtimestamp(time.time())
+print(time.time())
+
+st_time = time.strptime("2023-01-06 12:59:56", "%Y-%m-%d %H:%M:%S")
+print(type(st_time))  # <class 'time.struct_time'>
+raw_time = time.mktime(st_time)
+print(raw_time, type(raw_time))  # 1656864840.0 <class 'float'>
+
+dt = datetime.fromtimestamp(raw_time)
 ss = dt.strftime("%Y-%m-%d %H:%M:%S")
 print(ss)
