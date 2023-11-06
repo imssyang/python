1ce3c59272bed881b277f6aa55446d15ca61c580
imssyang <imssyang@gmail.com>
2023-11-06 17:43:27 +0800

    add design patterns to sample

--- sample/test/test1.py
+++ sample/test/test1.py
@@ -1,38 +1,24 @@
-import logging
-import sys
-
-class Logging:
-    def __init__(self, name=None):
+class A:
+    def __set_name__(self, owner, name):
+        print(
+            f"Descriptor instance assigned to attribute '{name}' of class '{owner.__name__}'"
+        )
         self.name = name
-        self.level = logging.INFO
-        self.fmt = "[%(levelname)1.1s %(asctime)s.%(msecs)03d %(threadName)s %(message)s"
-        self.datefmt = "%y%m%d %H:%M:%S"
-        logging.basicConfig(level=self.level, format=self.fmt, datefmt=self.datefmt)
-        self.add_handler(logging.getLogger(name), sys.stdout)
-        self.del_handler(logging.getLogger(name), sys.stderr)
-        self.list_logger()
 
-    def add_handler(self, logger, fd):
-        handler = logging.StreamHandler(fd)
-        handler.setLevel(self.level)
-        handler.setFormatter(logging.Formatter(self.fmt, self.datefmt))
-        logger.addHandler(handler)
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return instance.__dict__.get(self.name)
 
-    def del_handler(self, logger, fd):
-        for handler in logger.handlers:
-            if fd is handler.stream:
-                logger.removeHandler(handler)
-                break
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
 
-    def list_logger(self):
-        logger = logging.getLogger(self.name)
-        print(logger.handlers)
 
-if __name__ == "__main__":
-    Logging()
-    byte_string = '\xc3\xb4'
-    unicode_string = u'щ'
-    logging.info(f"11111")
-    logging.info(f"中文")
-    logging.info(f"{byte_string}")
-    logging.info(f"{unicode_string}")
+class B:
+    def __init__(self):
+        self.a = A()
+
+
+b = B()
+b.abc = 10
+print(b.abc)
--- sample/test/test2.py
+++ sample/test/test2.py
@@ -1,24 +1,68 @@
-# -*- coding: utf-8 -*-
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from typing import List, Dict
 
 
-class Test:
+class VXCodeRoute(object):
+    PREFER_GPU = False
+    ALLOW_EDGE = True
+    IS_PRIVILEGED = False
+    ROLE = ""
+    HIGH_IO = False
+    USERS_PRE_DEFAULT = ["all", "pre"]
+    USERS_RED_CHANNEL = ["vxcode_high_priority", "vxcode_red_channel"]
+
+    EXTERNAL_URLS = {
+        "bos": "10.227.0.13",
+        "kodo": "qiniucdn.com",
+        "ks3": "ksyun.com",
+        "cos": "myqcloud.com",
+    }
+
     @classmethod
-    def run(cls, urls):
-        paths = []
-        with ThreadPoolExecutor(max_workers=len(urls)) as executor:
-            workers = [
-                executor.submit(cls.transform, urls[index])
-                for index, _ in enumerate(urls)
-            ]
-            for fu in as_completed(workers):
-                paths.append(fu.result())
-        return paths
+    def is_vendor_stored(cls, url):
+        for i in cls.EXTERNAL_URLS.values():
+            if i in url:
+                return True
+        else:
+            return False
 
     @classmethod
-    def transform(cls, url):
-        return f"test-{url}"
+    def is_gpu_profiles(cls, profiles: List[Dict]) -> bool:
+        if profiles and profiles[0].get("arch") == "gpu":
+            return True
+        else:
+            return False
+
+    @classmethod
+    def route(cls, task_request, env=""):
+        task_buid = task_request["buid"]
+        red_channel = task_request.get("red_channel", False)
+        info = {
+            "users": [],
+        }
+        # pre环境新增一个 {"users":  ["all", "pre"]}
+        if env == "pre":
+            info["users"] += cls.USERS_PRE_DEFAULT
+        if red_channel:
+            info["users"] += cls.USERS_RED_CHANNEL
+        if "VJT" in task_buid:
+            info["is_privileged"] = True
+        if "GR:STREAM" in task_buid:
+            info["gpu_type"] = "xcode"
+            info["users"] += ["gpu_t4"]
+        return info
 
 
-paths = Test.run(["upos://sucaiboss/aaa.mov", "upos://sucaiboss/bbb.mov"])
-print(paths)
+print(VXCodeRoute.route({"buid": "GR:STREAM"}))
+print(VXCodeRoute.route({"buid": "test"}))
+print(VXCodeRoute.route({"buid": "test", "red_channel": True}))
+print(VXCodeRoute.route({"buid": "GR:STREAM"}))
+
+print(VXCodeRoute.route({"buid": "GR:STREAM"}, "pre"))
+print(VXCodeRoute.route({"buid": "test"}, "pre"))
+print(VXCodeRoute.route({"buid": "test", "red_channel": True}, "pre"))
+print(VXCodeRoute.route({"buid": "GR:STREAM"}, "pre"))
+
+print(VXCodeRoute.route({"buid": "GR:STREAM"}, "pre"))
+print(VXCodeRoute.route({"buid": "test"}, "pre"))
+print(VXCodeRoute.route({"buid": "test", "red_channel": True}, "pre"))
+print(VXCodeRoute.route({"buid": "GR:STREAM"}, "pre"))
