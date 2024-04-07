c4b50854c87b6daad6d5e5424a468aef395e1e42
imssyang <imssyang@gmail.com>
2024-04-07 16:47:11 +0800

    update setup and env

--- README.md
+++ README.md
@@ -246,19 +246,6 @@ https://www.riverbankcomputing.com/
 python -m http.server 8000 --bind 127.0.0.1 --directory /tmp/
 ```
 
-# Jupyterlab
-
-```bash
-$ python -c "from jupyter_server.auth import passwd; print(passwd('XXX'))"
-
-$ vi jupyter_lab_config.py
-c.ServerApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$nfU35Ct7vbjCiWxPhxLCMg$T1TSPT23ZvKfi7ykHioUSz/rbkvkTVClFWu5F7mZROQ'
-
-$ jupyter lab build --dev-build=False --minimize=False
-$ jupyter labextension uninstall @jupyterlab/celltags-extension
-```
-[Common Directories and File Locations](https://docs.jupyter.org/en/latest/use/jupyter-directories.html)
-
 # uwsgi
 
 ```bash
--- setup/env.sh
+++ setup/env.sh
@@ -6,26 +6,13 @@ if [[ -f "${PYENV_ROOT:-"$HOMEBREW_PREFIX"}/bin/pyenv" ]]; then
   eval "$(pyenv init - zsh)"
 fi
 
-#package_paths=(
-#  /usr/local/lib/python3.8/site-packages
-#  /opt/python3/lib/python3.8/site-packages
-#  /opt/python3/sample
-#  /root/.blog/flask/source
-#  $PYTHONPATH
-#)
-#IFS=:$IFS; export PYTHONPATH="${package_paths[*]}"; IFS=${IFS#?}
-#export PYTHONWARNINGS="ignore::DeprecationWarning"
-#export PYTHONNOUSERSITE=1
-#eval "optbin -s /opt/python3/bin"
-#eval "optman -s /opt/python3/share/man"
-#eval "optpkg -s /opt/python3/lib/pkgconfig"
-#eval "optlib -s /opt/python3/lib"
-#eval "optlib -s /opt/python3/lib/python3.8/config-3.8-x86_64-linux-gnu"
-#eval "optlib -s /opt/python3/lib/python3.8/lib-dynload"
-#eval "optlib -s /opt/python3/lib/python3.8/site-packages"
-
-# pip
-#export PIP_CONFIG_FILE=/opt/python3/conf/pip.conf
+package_paths=(
+  /opt/python/sample
+  $PYTHONPATH
+)
+IFS=:$IFS; export PYTHONPATH="${package_paths[*]}"; IFS=${IFS#?}
+export PYTHONWARNINGS="ignore::DeprecationWarning"
+export PYTHONNOUSERSITE=1
 
 # pipx
 #export PIPX_BIN_DIR=/opt/python3/bin
@@ -47,14 +34,6 @@ fi
 
 # pytest
 #export PYTEST_ADDOPTS="-vv --disable-pytest-warnings --durations=0 -s"
-
-# flask
-#export FLASK_DEBUG=1
-
-# jupyter
-#export JUPYTER_CONFIG_DIR=/opt/python3/conf/jupyter
-#export JUPYTER_DATA_DIR=/opt/python3/data/jupyter
-#export JUPYTER_RUNTIME_DIR=/opt/python3/data/jupyter/runtime
 
 # alias
 #alias uwsgitop="uwsgitop localhost:2031"
